132566950eb1834826ae3c2cf202fcefbde9bb89
Markus Kristoffer Sibbesen <mk@sibbesen.dk>
2024-12-09 08:07:35 +0100

    first part of gridsearch (finding l1 lambda equivalent to k)

--- src/sparse_autoencoders.py
+++ src/sparse_autoencoders.py
@@ -2,7 +2,8 @@ import torch
 from torch import nn
 import einops
 
-class SAE_topk(nn.Module):
+class SAE(nn.Module):
+
     def __init__(self, meta_data:dict):
         super().__init__()
 
@@ -10,55 +11,69 @@ class SAE_topk(nn.Module):
 
         input_size = meta_data["input_size"]
         hidden_size = meta_data["hidden_size"]
-        self.k = meta_data['k']
         self.meta_data = meta_data
 
-        self.pre_encode_b = nn.Parameter(torch.randn(hidden_size)*0.1)
-
         initial_W = torch.randn(hidden_size, input_size) * 0.01
-        
         with torch.no_grad():
             self.W = nn.Parameter(initial_W.clone())
             self.WT = nn.Parameter(initial_W.T.clone())
 
-
+        if meta_data["pre_encoder_bias"]:
+            self.pre_encode_b = nn.Parameter(torch.randn(hidden_size)*0.1)
         self.b1 = nn.Parameter(torch.randn(hidden_size)*0.1)  # Bias for encoder
-        self.b2 = nn.Parameter(torch.randn(input_size)*0.1)  # Bias for decoder
-        self.hidden_activations = None
+
+        self.activations = None
+
+    def get_preacts(self, x):
+        if self.meta_data["pre_encoder_bias"]:
+            x = x - self.pre_encode_b
+
+        return torch.matmul(x, self.WT) + self.b1
+
+
+
+class SAE_topk(SAE):
 
     def forward(self, x):
         if self.meta_data["pre_encoder_bias"]:
-            x = x - self.b2
+            x = x - self.pre_encode_b
 
-        if self.meta_data["activation_function"] == "topk":
-            h = torch.topk(torch.matmul(x, self.WT) + self.b1, k=self.k, dim=-1)
-            self.hidden_activations = h
-            self.active_neurons = len(torch.unique(h.indices))
-            x_hat = einops.einsum(h.values, self.W[h.indices], 'token topk, token topk out -> token out') + self.b2
-        else:
-            h = torch.relu(torch.matmul(x, self.WT) + self.b1)
-            self.hidden_activations = h 
-            
+        h = torch.topk(torch.matmul(x, self.WT) + self.b1, k=self.k, dim=-1)
+        self.activations = h
+        self.active_neurons = len(torch.unique(h.indices))
+        x_hat = einops.einsum(h.values, self.W[h.indices], 'token topk, token topk out -> token out')
 
-#            acts = sae_trainers[0].model.hidden_activations
-            self.active_neurons = sum(h.sum(dim=0) > 0.001)
-            #self.active_neurons = torch.sum(h > 0).item()
-
-            x_hat = torch.matmul(h, self.W) + self.b2
+        if self.meta_data['pre_encoder_bias']:
+            x_hat += self.pre_encode_b
 
         return x_hat
 
     def get_activations(self, x):
-        
-        if self.meta_data["activation_function"] == "topk":
-            h = torch.topk(torch.matmul(x, self.WT) + self.b1, k=self.k, dim=-1)
-        else:
-            h = torch.relu(torch.matmul(x, self.WT) + self.b1)
-
+        h = torch.topk(torch.matmul(x, self.WT) + self.b1, k=self.k, dim=-1)
         return h
     
-    def get_preacts(self, x):
-        if self.meta_data["pre_encoder_bias"]:
-            x = x - self.b2
 
-        return torch.matmul(x, self.WT) + self.b1
+    
+
+class SAE_RELU(SAE):
+
+    def forward(self, x):
+        if self.meta_data["pre_encoder_bias"]:
+            x = x - self.pre_encode_b
+
+        h = torch.relu(torch.matmul(x, self.WT) + self.b1)
+        self.activations = h
+        self.active_neurons_per_batch = sum(h.sum(dim=0) > 0.001)
+        #self.active_neurons = torch.sum(h > 0).item()
+
+        x_hat = torch.matmul(h, self.W)
+        
+        if self.meta_data['pre_encoder_bias']:
+            x_hat += self.pre_encode_b
+
+        return x_hat
+    
+    def get_activations(self, x):
+        h = torch.relu(torch.matmul(x, self.WT) + self.b1)
+        return h
+
